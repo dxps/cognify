7d37262963315b151c47135340f4db5ac3960e06
dxps <marius.ileana@gmail.com>
2025-10-17 12:20:25 +0300

    ui: login page

--- web-ui/app/page.tsx
+++ web-ui/app/page.tsx
@@ -1,12 +1,37 @@
+import { LoginButton } from '@/components/auth/login-button'
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { Poppins } from 'next/font/google'
+
+const font = Poppins({
+	subsets: ['latin'],
+	weight: ['600'],
+})
 
 export default function Home() {
 	return (
-		<>
-			<p>Hello</p>
-			<Button variant="default" className="cursor-pointer">
-				Button
-			</Button>
-		</>
+		<main className="flex h-full flex-col items-center justify-center bg-slate-200">
+			<div className="space-y-6">
+				<h1
+					className={cn(
+						'text-4xl font-semibold text-slate-800 drop-shadow-md',
+						font.className
+					)}
+				>
+					Auth
+				</h1>
+				<div>
+					<LoginButton>
+						<Button
+							variant="secondary"
+							size="lg"
+							className="cursor-pointer drop-shadow-sm"
+						>
+							Sign in
+						</Button>
+					</LoginButton>
+				</div>
+			</div>
+		</main>
 	)
 }
---
+++ web-ui/components/auth/social.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { FcGoogle } from 'react-icons/fc'
+import { Button } from '../ui/button'
+import { FaGithub } from 'react-icons/fa'
+
+const Social = () => {
+	return (
+		<div className="flex items-center w-full gap-x-2">
+			<Button
+				variant="outline"
+				size="lg"
+				className="w-1/2 cursor-pointer"
+				onClick={() => {}}
+			>
+				<FcGoogle className="h-5 w-5" />
+			</Button>
+			<Button
+				variant="outline"
+				size="lg"
+				className="w-1/2 cursor-pointer"
+				onClick={() => {}}
+			>
+				<FaGithub className="h-5 w-5" />
+			</Button>
+		</div>
+	)
+}
+
+export default Social

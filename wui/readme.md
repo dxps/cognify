df59477f930faa9995d66a1476b4c4436ff31609
dxps <marius.ileana@gmail.com>
2025-10-29 11:38:24 +0200

    wui: add db & drizzle foundation
    Used [this doc](https://orm.drizzle.team/docs/get-started/bun-sql-new) from Drizzle.

--- wui/readme.md
+++ wui/readme.md
@@ -8,8 +8,14 @@ This project was created using `bun init` in [Bun v1.3.1](https://bun.com), usin
 
 ### Setup
 
-As prerequisites, you need to have [Bun](https://bun.sh/) installed.<br/>
-Next, run `bun install` to install the project dependencies.
+Being a Bun based project, you need to have [Bun](https://bun.sh/) installed.<br/>
+
+-   Run `bun install` to install the project dependencies.
+-   Run `docker compose up -d` to start the PostgreSQL database instance as a Docker container.
+-   Create a `.env` file (see `.env.example` for more information).
+
+Additionally, you can apply the changes to the database by running `bun db:push`.<br/>
+This is a convenient method for quickly testing new schema designs or modifications in a local development environment, allowing for rapid iterations without the need to [manage migration](https://orm.drizzle.team/docs/kit-overview) files.
 
 <br/>
 
--- wui/src/index.ts
+++ wui/src/index.ts
@@ -1,41 +1,89 @@
-import { serve } from "bun";
-import index from "./index.html";
+import { serve } from 'bun'
+import index from './index.html'
 
-const server = serve({
-  routes: {
-    // Serve index.html for all unmatched routes.
-    "/*": index,
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/bun-sql'
+import { eq } from 'drizzle-orm'
+import { usersTable } from './db/schema'
 
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
+const db = drizzle({
+	connection: {
+		url: process.env.DATABASE_URL!,
+		// Max number of connections.
+		max: 2,
+		// Maximum number of seconds to keep a connection alive.
+		maxLifetime: 180,
+	},
+})
 
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
-  },
+main()
 
-  development: process.env.NODE_ENV !== "production" && {
-    // Enable browser hot reloading in development
-    hmr: true,
+async function main() {
+	await dbInitialCheck()
+	startServer()
+}
 
-    // Echo console logs from the browser to the server
-    console: true,
-  },
-});
+async function dbInitialCheck() {
+	const user: typeof usersTable.$inferInsert = {
+		name: 'John',
+		age: 30,
+		email: 'john@doe.com',
+	}
+	await db.insert(usersTable).values(user)
+	console.log('🧪 New user created!')
 
-console.log(`🚀 Server running at ${server.url}`);
+	const users = await db.select().from(usersTable)
+	console.log('🧪 Getting all users from the database: ', users)
+
+	await db
+		.update(usersTable)
+		.set({
+			age: 31,
+		})
+		.where(eq(usersTable.email, user.email))
+	console.log('🧪 User info updated!')
+
+	await db.delete(usersTable).where(eq(usersTable.email, user.email))
+	console.log('🧪 User deleted!')
+}
+
+function startServer() {
+	const server = serve({
+		routes: {
+			// Serve index.html for all unmatched routes.
+			'/*': index,
+
+			'/api/hello': {
+				async GET(req) {
+					return Response.json({
+						message: 'Hello, world!',
+						method: 'GET',
+					})
+				},
+				async PUT(req) {
+					return Response.json({
+						message: 'Hello, world!',
+						method: 'PUT',
+					})
+				},
+			},
+
+			'/api/hello/:name': async (req) => {
+				const name = req.params.name
+				return Response.json({
+					message: `Hello, ${name}!`,
+				})
+			},
+		},
+
+		development: process.env.NODE_ENV !== 'production' && {
+			// Enable browser hot reloading in development
+			hmr: true,
+
+			// Echo console logs from the browser to the server
+			console: true,
+		},
+	})
+
+	console.log(`🚀 Server running at ${server.url}`)
+}
